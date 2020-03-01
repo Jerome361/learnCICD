a85c44e64a070a49e3d94a78c16f4e42f960d461
Tumwesigye <Tumwesigye>
2020-03-01 13:35:05 +0300

    debugged marks file

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const marks = require("./models/marks");
 const student = require("./models/student");
 
-const sandraGrade = new marks("SST",-60);
+const sandraGrade = new marks("SST","Sandra",60);
 sandraGrade.computeGrade();
-sandraGrade.isValidate()
 
 const hajiGrade = new marks("Islam","Haruna",90);
 hajiGrade.computeGrade();
-hajiGrade.isValidate();
+
--- test/testgrade.test.js
+++ test/testgrade.test.js
@@ -1,8 +1,12 @@
-const mark = require("../index");
+//const mark = require("../index");
+const marks = require("../models/marks");
+
+const testGrade = new marks();
 
 test("testing the mark is a number", () => {
-  expect(mark.isValidate()).toBe(true);
+  expect(testGrade.isValidate()).toBe(false);
 });
+
 test("testing the grade mark", () => {
-  expect(mark.computeGrade()).toBe("C");
+  expect(testGrade.computeGrade()).toBe(undefined);
 });
